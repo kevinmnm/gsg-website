015640c49dd12f18c8489071cca7dd7b803574ba
Kevin Song <kevinmnm2010@gmail.com>
2020-09-14 19:45:58 -0400

    add slide effect function

--- src/view/Home.js
+++ src/view/Home.js
@@ -2,12 +2,32 @@ import React, { useState, useEffect } from 'react';
 import './Home.css';
 
 const Home = () => {
-   let [home_img, set_home_img] = useState('home_large_1.png');
+   const first = 'home_large_1.png';
+   const second = 'home_large_2.png';
+   const third = 'home_large_3.png';
+
+   let [home_img, set_home_img] = useState(first);
+
+   const slider = (e) => {
+      if (e.target.classList.contains('arrow-left')){
+         home_img === first ? set_home_img(third)
+         : home_img === second ? set_home_img(first)
+         : set_home_img(second);
+      } else {
+         home_img === first ? set_home_img(second)
+         : home_img === second ? set_home_img(third)
+         : set_home_img(first);
+      }
+   }
 
    return (
       <div id='home'>
          <div className='home-img-wrap'>
-            <img className='home-img' src={require(`../assets/${home_img}`)} />
+            <img className='home-img' src={require(`../assets/${home_img}`)} draggable='false' />
+            <div className='home-arrow-wrap'>
+               <div className='arrow arrow-left' onClick={slider}>&#8249;</div>
+               <div className='arrow arrow-right' onClick={slider}>&#8250;</div>
+            </div>
          </div>
       </div>
    );
