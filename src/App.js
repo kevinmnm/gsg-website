05e91cba52b85ccc23d5d0ec278142e8a4af10a8
Kevin Song <kevinmnm2010@gmail.com>
2020-09-14 19:06:35 -0400

    fix css upon component adjustment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-//import Nav from './Nav.js';
 import Home from './view/Home.js'
 
 class App extends React.Component {
@@ -8,8 +7,8 @@ class App extends React.Component {
       nav_expand: false
    }
 
-   nav_slide = ()=>{
-      this.setState( prevState => {
+   nav_slide = () => {
+      this.setState(prevState => {
          return {
             nav_expand: !prevState.nav_expand
          }
@@ -18,22 +17,24 @@ class App extends React.Component {
 
    render() {
       return (
-         <div id='nav'>
-            <div className='logo-wrap'>
-               <img className='logo' src={require('./assets/logo.png')} alt='logo.png' />
-            </div>
-            <div className='nav-list'>
-               <div>HOME</div>
-               <div>ABOUT</div>
-               <div>HARDWARE</div>
-               <div>SOFTWARE</div>
-            </div>
-            <div className='sign-in'>Sign In</div>
-            {/* <div className={this.state.nav_expand ? 'container change' : 'container'} onClick={this.nav_slide}> */}
-            <div className={this.state.nav_expand ? 'container change' : 'container'} onClick={this.nav_slide}>
-               <div className="bar1"></div>
-               <div className="bar2"></div>
-               <div className="bar3"></div>
+         <div id='app'>
+            <div id='nav'>
+               <div className='logo-wrap'>
+                  <img className='logo' src={require('./assets/logo.png')} alt='logo.png' />
+               </div>
+               <div className='nav-list'>
+                  <div>HOME</div>
+                  <div>ABOUT</div>
+                  <div>HARDWARE</div>
+                  <div>SOFTWARE</div>
+               </div>
+               <div className='sign-in'>Sign In</div>
+               {/* <div className={this.state.nav_expand ? 'container change' : 'container'} onClick={this.nav_slide}> */}
+               <div className={this.state.nav_expand ? 'container change' : 'container'} onClick={this.nav_slide}>
+                  <div className="bar1"></div>
+                  <div className="bar2"></div>
+                  <div className="bar3"></div>
+               </div>
             </div>
 
             <Home />
