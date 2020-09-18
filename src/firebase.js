8be11c1463baf8bd081d72f16be8599c89e217b0
Kevin Song <kevinmnm2010@gmail.com>
2020-09-18 19:03:56 -0400

    add firebase to the project

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyC7W0DtckKvLYnujcO0S0fkvdYC5OY8sgg",
+   authDomain: "gaming-setup-guide.firebaseapp.com",
+   databaseURL: "https://gaming-setup-guide.firebaseio.com",
+   projectId: "gaming-setup-guide",
+   storageBucket: "gaming-setup-guide.appspot.com",
+   messagingSenderId: "451589339562",
+   appId: "1:451589339562:web:0a0d4b3e3e6265f9cc13d3",
+   measurementId: "G-0PZ4X1VNGP"
+};
+
+export default firebaseConfig;
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
