67675bfaa3efc354c3eda455f130313196acd7c9
Kevin Song <kevinmnm2010@gmail.com>
2020-09-19 19:46:51 -0400

    add harware page

--- src/view/Hardware.js
+++ src/view/Hardware.js
@@ -6,7 +6,20 @@ class Hardware extends React.Component {
    render() {
       return (
          <div id='Hardware'>
-            <h1>Hardwares</h1>
+            <h1><u>Hardwares</u></h1>
+            <div className='hardware-content-wrap'>
+               {
+                  hardware_list.map( product => {
+                     return (
+                        <div className='hardware-content' key={product.type}>
+                           <div className='content-title'>{product.type}</div>
+                           <img src={require('../assets/'+product.img)} alt='Product Image' />
+                           <div className='content-spec'>{product.spec}</div>
+                        </div>
+                     )
+                  })
+               }
+            </div>
          </div>
       );
    }
