235a9e170607f26e682f5c62ea5bde677163c0dc
Kevin Song <kevinmnm2010@gmail.com>
2020-09-19 19:27:23 -0400

    recommenation product

--- src/view/Hardware.js
+++ src/view/Hardware.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Hardware.css';
+import hardware_list from './component/hardware-list.js';
 
 class Hardware extends React.Component {
    render() {
       return (
          <div id='Hardware'>
-            <h1>This is Hardware page</h1>
+            <h1>Hardwares</h1>
          </div>
       );
    }
---
+++ src/view/component/hardware-list.js
@@ -0,0 +1,19 @@
+const hardware_list = [
+   {
+      type: 'CPU',
+      spec: 'Intel Core i9-9900K Coffee Lake 8-Core, 16-Thread, 3.6 GHz (5.0 GHz Turbo) LGA 1151 (300 Series) 95W BX806849900K Desktop Processor Intel UHD Graphics 630',
+      img: 'CPU_img.jpg'
+   },
+   {
+      type: 'MOTHERBOARD',
+      spec: 'ASUS Prime Z390-A LGA 1151 (300 Series) ATX Intel Motherboard',
+      img: 'motherboard_img.jpg'
+   },
+   {
+      type: 'GRAPHICS CARD',
+      spec: 'MSI GeForce RTX 2060 DirectX 12 RTX 2060 VENTUS XS 6G OC Video Card',
+      img: 'graphics_img.jpg'
+   }
+];
+
+export default hardware_list;
