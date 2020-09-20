3795046de48540e38cef948aac17ec42ae291691
Kevin Song <kevinmnm2010@gmail.com>
2020-09-19 20:16:34 -0400

    add software page configuration

--- src/view/Software.js
+++ src/view/Software.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Software.css';
+import software_list from './component/software-list.js';
 
 class Software extends React.Component {
    render() {
       return (
          <div id='Software'>
-            <h1>Software Page</h1>
+            <h1><u>Software</u></h1>
+            <div className='software-content-wrap'>
+               {
+                  software_list.map(product => {
+                     return (
+                        <div className='software-content'>
+                           <div className='software-content-title'>{product.title}</div>
+                           <img src={require('../assets/' + product.img)} alt='Software Image' />
+                           <div className='software-content-desc'>{product.desc}</div>
+                        </div>
+                     )
+                  })
+               }
+            </div>
          </div>
       );
    }
